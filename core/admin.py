1740902d4fdfa52c95cc567a53ccfb6fb33209e7
Jacob Lal <jacobandeth@gmail.com>
2025-11-11 10:56:26 +0530

    updated admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'owner', 'created_at')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('genre', 'created_at')
