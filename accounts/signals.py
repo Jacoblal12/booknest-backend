316ae60206aecab477a992611fe6656ae4f2be4b
Jacob Lal <jacobandeth@gmail.com>
2025-11-21 13:40:59 +0530

    modified signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,13 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Profile
 
+User = get_user_model()
+
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create a Profile row automatically when a new User is created."""
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+def save_user_profile(sender, instance, **kwargs):
+    """Save the related Profile if it exists (safe: won't crash if missing)."""
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
